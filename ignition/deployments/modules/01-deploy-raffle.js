8decf7ce4c17cade6f6b1c383a23393271566515
Enrique Folte <hhfolte@gmail.com>
2025-01-07 14:20:47 -0300

    complete ignition test documentation and/or possible later work with ethers v6

--- ignition/deployments/modules/01-deploy-raffle.js
+++ ignition/deployments/modules/01-deploy-raffle.js
@@ -6,22 +6,28 @@ const { network } = require("hardhat")
 const chainId = network.config.chainId
 const { ethers } = require("hardhat")
 
-module.exports = buildModule("Raffle", (m) => {
-    const vrfCoordinatorV2 = "0x1234.." // address to request random value from chainlink
-    const subscriptionId = 666
-    const gasLane = "0x1234.." // 500 Gwei
-    const interval = 60
-    const entranceFee = ethers.parseEther("0.01") // Replace with the entrance fee in ETH
-    const callbackGasLimit = 500000
-    const _feeRecipient = "0x1234.."
+module.exports = buildModule(
+    "Raffle",
+    (m) => {
+        const vrfCoordinatorV2 = "0x1234.." // address to request random value from chainlink
+        const subscriptionId = 666
+        const gasLane = "0x1234.." // 500 Gwei
+        const interval = 60
+        const entranceFee = ethers.parseEther("0.01") // Replace with the entrance fee in ETH
+        const callbackGasLimit = 500000
+        const _feeRecipient = "0x1234.."
 
-    return {
-        vrfCoordinatorV2,
-        subscriptionId,
-        gasLane,
-        interval,
-        entranceFee,
-        callbackGasLimit,
-        _feeRecipient,
-    }
-})
+        return {
+            vrfCoordinatorV2,
+            subscriptionId,
+            gasLane,
+            interval,
+            entranceFee,
+            callbackGasLimit,
+            _feeRecipient,
+        }
+    },
+    {
+        tags: ["all", "raffle"],
+    },
+)
