c19fdf7a9babb0243b192f4717351bcb60d3573d
Enrique Folte <hhfolte@gmail.com>
2025-01-07 08:41:47 -0300

    add more verbose network configuration to helper-hardhat-config.js

--- helper-hardhat-config.js
+++ helper-hardhat-config.js
@@ -10,8 +10,21 @@ const networkConfig = {
         callbackGasLimit: "500000",
         interval: "60",
     },
+    1: {
+        name: "mainnet",
+        interval: "30",
+    },
 }
+
+const developmentChains = ["hardhat", "localhost"]
+const VERIFICATION_BLOCK_CONFIRMATIONS = 6
+const frontEndContractsFile = "../nextjs-smartcontract-lottery-fcc/constants/contractAddresses.json"
+const frontEndAbiFile = "../nextjs-smartcontract-lottery-fcc/constants/abi.json"
 
 module.exports = {
     networkConfig,
+    developmentChains,
+    VERIFICATION_BLOCK_CONFIRMATIONS,
+    frontEndContractsFile,
+    frontEndAbiFile,
 }
